85409c7175985485e53c3ddf7fd0a8b7222f2443
Julian Sanchez <julian.sanchez@silstech.com.ar>
2022-04-05 02:23:50 -0300

    Metodo para calcular puntaje segun puesto en hackaton

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,14 @@
-export const randomIntFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+/**
+ * Function recieves hackaton position and returns the ranking position points.
+ * @param {number} position
+ * @returns number
+ */
+export const calculateHackatonPoints = (position) => {
+  const points = {
+    1: 100,
+    2: 90,
+    3: 80
+  };
+
+  return points[position];
 };
