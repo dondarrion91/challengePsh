0c5e5a167a28c47be7a79a8c4e021ae2d7874180
Julian Sanchez <julian.sanchez@silstech.com.ar>
2022-04-06 19:44:20 -0300

    Se agrega styled components del Home

---
+++ client/src/views/Home/style/styled-components.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: 100vh;
+`;
+
+export const Img = styled.img`
+  width: 80%;
+  height: 100%;
+  object-fit: scale-down;
+`;
