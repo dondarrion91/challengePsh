de98175693244476a0155aa03e4cac94a5fc5ee4
Julian Sanchez <julian.sanchez@silstech.com.ar>
2022-04-06 19:44:46 -0300

    Se agrega links de vistas en header

--- client/src/components/shared/NavBar.jsx
+++ client/src/components/shared/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Nav, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
@@ -12,8 +13,18 @@ export default function NavBar() {
           className="d-inline-block align-top ms-4"
           alt="hackaton site logo"
         />
-        <span className="text-white fw-bold ms-4">WORLD HACKATHON SITE</span>
+        <Link className="no-underline" to="/">
+          <span className="text-white fw-bold ms-4">WORLD HACKATHON SITE</span>
+        </Link>
       </Navbar.Brand>
+      <Nav>
+        <Link className="nav-link" to="/hackathon-list">
+          Hackathons list
+        </Link>
+        <Link className="nav-link" to="/world-best-developers">
+          World best developers
+        </Link>
+      </Nav>
       <Navbar className="ms-auto me-3" id="basic-navbar-nav">
         <Nav>
           <Button variant="primary">Login</Button>
