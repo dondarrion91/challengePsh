7a050d3d0beb3a01ce1a0a6a10d1efe54c3fa02c
Julian Sanchez <julian.sanchez@silstech.com.ar>
2022-04-07 03:17:54 -0300

    Cookie parser

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 import "./src/utils/jobs";
 import router from "./src/routes/index";
+import cookieParser from "cookie-parser";
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(cors());
 
